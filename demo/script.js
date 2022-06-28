0dd1287667b0263f6b26a50766bfdb3ddc6e6b96
hyunjin <leehj0110@kakao.com>
2022-06-28 23:21:51 +0900

    feat: roomId로 1대1 채팅 구현 (#3)

--- demo/script.js
+++ demo/script.js
@@ -3,49 +3,28 @@ const messageContainer = document.querySelector("#message-container")
 const messageForm = document.getElementById("send-container")
 const messageInput = document.getElementById("message-input")
 
-console.log(socket)
-const name = prompt("닉네임을 입력해주세요.")
 appendMessage("참여하셨습니다.")
 
-// socket.emit("new-user", name)
+let room
 
-socket.emit("personal-conn")
+socket.emit("random-join")
+socket.on("joined-room", (roomId) => {
+  console.log("roomId", roomId)
 
-socket.on("chat-message", (data) => {
-  appendMessage(`${data.name}: ${data.message}`)
+  room = roomId
 })
 
-socket.on("user-connected", (name) => {
-  appendMessage(`${name} connected`)
-})
-
-socket.on("user-disconnected", (name) => {
-  appendMessage(`${name} disconnected`)
-})
-
-// socket.on("personal-connection", (a) => {
-//   console.log(a)
-// })
-
-socket.on("conn", (id) => {
-  console.log(id)
-
-  socket.emit("personal-chat", id)
-})
-
-socket.on("wait", (message) => {
-  console.log("ㄱㄷ")
-})
-
-socket.on("personal-connection", (id) => {
-  console.log(id, "상대방 id")
+socket.on("receive-message", (message) => {
+  console.log(message)
+  appendMessage(message)
 })
 
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault()
   const message = messageInput.value
   appendMessage(`You: ${message}`)
-  socket.emit("send-chat-message", message)
+  // socket.emit("send-chat-message", message)
+  socket.emit("send-message", room, message)
   messageInput.value = ""
 })
 
@@ -54,3 +33,35 @@ function appendMessage(message) {
   messageElement.innerText = message
   messageContainer.appendChild(messageElement)
 }
+
+// socket.emit("personal-conn")
+
+// socket.on("chat-message", (data) => {
+//   appendMessage(`${data.name}: ${data.message}`)
+// })
+
+// socket.on("user-connected", (name) => {
+//   appendMessage(`${name} connected`)
+// })
+
+// socket.on("user-disconnected", (name) => {
+//   appendMessage(`${name} disconnected`)
+// })
+
+// socket.on("personal-connection", (a) => {
+//   console.log(a)
+// })
+
+// socket.on("conn", (id) => {
+//   console.log(id)
+
+//   socket.emit("personal-chat", id)
+// })
+
+// socket.on("wait", (message) => {
+//   console.log("ㄱㄷ")
+// })
+
+// socket.on("personal-connection", (id) => {
+//   console.log(id, "상대방 id")
+// })
--- demo/server.js
+++ demo/server.js
@@ -1,74 +1,115 @@
 const io = require("socket.io")(3000, { cors: { origins: "*" } })
+const { v4: uuidv4 } = require("uuid")
 console.log("server started at port 3000")
 
 const users = {}
 const rooms = {}
 
 io.on("connection", (socket) => {
-  socket.on("new-user", (name) => {
-    users[socket.id] = name
-    console.log(users)
-    socket.broadcast.emit("user-connected", name)
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId)
   })
 
-  socket.on("send-chat-message", (message) => {
-    socket.broadcast.emit("chat-message", {
-      message: message,
-      name: users[socket.id],
-    })
+  socket.on("send-message", (roomId, message) => {
+    socket.to(roomId).emit("receive-message", message)
   })
 
-  // io.to(socket.id).emit("conn", socket.id)
-
-  socket.on("personal-chat", (id) => {
-    console.log(id, "아마존")
-
+  socket.on("random-join", () => {
     for (let room in rooms) {
       if (rooms[room].length === 1) {
-        rooms[room].push(id)
+        rooms[room].push(socket.id)
+        socket.join(room)
+        socket.emit("joined-room", room)
 
-        console.log(rooms[room][0], "사용자1")
-        console.log(rooms[room][1], "사용자2")
-        io.to(rooms[room][0]).emit("personal-connection", rooms[room][1])
-        io.to(rooms[room][1]).emit("personal-connection", rooms[room][0])
+        // io.to(rooms[room][0]).emit("personal-connection", room)
+        // io.to(rooms[room][1]).emit("personal-connection", room)
         return
       }
     }
 
-    const roomCount = Object.keys(rooms).length
-    const roomId = roomCount
-
-    rooms[roomId] = [socket.id]
-    io.to(socket.id).emit("wait")
-    // 개인 1대1 연결을 원하는 사용자가 들어오면 방을 순회하면서 방의 인원수가 하나인 방을 리턴?
-    // const roomCount = Object.keys(rooms).length
-    // if (roomCount === 0) {
-    //   rooms[0] = [socket.id]
-    //   return
-    // }
-    // // 방찾기
-    // for (let room in rooms) {
-    //   if (room.length === 1) {
-    //     room.push(socket.id)
-    //     io.to(room[0]).emit("personal-connection", room[1])
-    //     io.to(room[1]).emit("personal-connection", room[0])
-    //     return
-    //   }
-    // }
-    // // 방생성
     // const roomId = Object.keys(rooms).length
     // rooms[roomId] = [socket.id]
-    // if (id in rooms) {
-    //   rooms[id] = socket.id
-    // }
+    const roomId = uuidv4()
+    rooms[roomId] = [socket.id]
+
+    socket.join(roomId)
+    socket.emit("joined-room", roomId)
   })
 
-  socket.on("personal-conn", () => {
-    io.to(socket.id).emit("conn", socket.id)
-  })
+  // socket.on("new-user", (name) => {
+  //   users[socket.id] = name
+  //   console.log(users)
+  //   socket.broadcast.emit("user-connected", name)
+  // })
+
+  // socket.on("send-chat-message", (message) => {
+  //   socket.broadcast.emit("chat-message", {
+  //     message: message,
+  //     name: users[socket.id],
+  //   })
+  // })
+
+  // io.to(socket.id).emit("conn", socket.id)
+
+  // socket.on("personal-chat", (id) => {
+  //   console.log(id, "아이디에요")
+
+  //   for (let room in rooms) {
+  //     if (rooms[room].length === 1) {
+  //       rooms[room].push(id)
+
+  //       console.log(rooms[room][0], "사용자1")
+  //       console.log(rooms[room][1], "사용자2")
+  //       // io.to(rooms[room][0]).emit("personal-connection", rooms[room][1])
+  //       // io.to(rooms[room][1]).emit("personal-connection", rooms[room][0])
+
+  //       // 방 번호를 보내준다.
+  //       io.to(rooms[room][0]).emit("personal-connection", room)
+  //       io.to(rooms[room][1]).emit("personal-connection", room)
+  //       return
+  //     }
+  //   }
+
+  //   const roomCount = Object.keys(rooms).length
+  //   const roomId = roomCount
+
+  //   rooms[roomId] = [socket.id]
+  //   io.to(socket.id).emit("wait")
+  // 개인 1대1 연결을 원하는 사용자가 들어오면 방을 순회하면서 방의 인원수가 하나인 방을 리턴?
+  // const roomCount = Object.keys(rooms).length
+  // if (roomCount === 0) {
+  //   rooms[0] = [socket.id]
+  //   return
+  // }
+  // // 방찾기
+  // for (let room in rooms) {
+  //   if (room.length === 1) {
+  //     room.push(socket.id)
+  //     io.to(room[0]).emit("personal-connection", room[1])
+  //     io.to(room[1]).emit("personal-connection", room[0])
+  //     return
+  //   }
+  // }
+  // // 방생성
+  // const roomId = Object.keys(rooms).length
+  // rooms[roomId] = [socket.id]
+  // if (id in rooms) {
+  //   rooms[id] = socket.id
+  // }
+  // })
+
+  // socket.on("personal-conn", () => {
+  //   io.to(socket.id).emit("conn", socket.id)
+  // })
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("user-disconnected", users[socket.id])
-    delete users[socket.id]
+    // socket.broadcast.emit("user-disconnected", users[socket.id])
+    // delete users[socket.id]
+
+    for (let room in rooms) {
+      if (rooms[room].includes(socket.id)) {
+        delete rooms[room]
+      }
+    }
   })
 })
