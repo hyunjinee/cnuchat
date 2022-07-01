7e8b1813a454f258be8fe87a1c52b1eed1cfbe9c
hyunjin <leehj0110@kakao.com>
2022-07-01 21:41:38 +0900

    chore: styled component 오류 해결

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,34 +1,26 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
-} from 'next/document';
+import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: (
-          <>
+        styles: [
+          // https://velog.io/@junho0956/NextJS-styled-component
+          <React.Fragment key={'key'}>
             {initialProps.styles}
             {sheet.getStyleElement()}
-          </>
-        ),
+          </React.Fragment>,
+        ],
       };
     } finally {
       sheet.seal();
@@ -43,13 +35,10 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
           <meta property="og:title" content="충남대학교 채팅" />
           <meta property="og:image" content="" />
-          <meta
-            property="og:description"
-            content="충남대학교 학생들과 대화를 나눌 수 있는 플랫폼입니다."
-          />
+          <meta property="og:description" content="충남대학교 학생들과 대화를 나눌 수 있는 플랫폼입니다." />
           <meta property="og:url" content="/" />
-          <meta name="description" content="" />
-          <meta name="keywords" content="" />
+          <meta name="description" content="충남대학교 학생들과 즐거운 대화를 나눠보세요!" />
+          <meta name="keywords" content="충남대, 학생" />
         </Head>
         <body>
           <Main />
